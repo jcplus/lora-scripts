f99825a8ad9b53e0bcab3b8d4078c0ae2ef434f0
SevenTree <jcplus@gmail.com>
2023-08-04 15:36:52 +1000

    Update install.bash

--- install.bash
+++ install.bash
@@ -30,19 +30,8 @@ cuda_minor_version=$(echo "$cuda_version" | awk -F'.' '{print $2}')
 
 echo "Cuda Version:$cuda_version"
 
-if (( cuda_major_version >= 12 )) || (( cuda_major_version == 11 && cuda_minor_version >= 8 )); then
-    echo "install torch 2.0.1+cu118"
-    pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-    pip install xformers==0.0.19
-elif (( cuda_major_version == 11 && cuda_minor_version == 6 )); then
-    echo "install torch 1.12.1+cu116"
-    pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
-    pip install --upgrade git+https://github.com/facebookresearch/xformers.git@0bad001ddd56c080524d37c84ff58d9cd030ebfd
-    pip install triton==2.0.0.dev20221202
-else
-    echo "Unsupported cuda version:$cuda_version"
-    exit 1
-fi
+pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip install xformers==0.0.19
 
 echo "Installing deps..."
 cd "$script_dir/sd-scripts" || exit
